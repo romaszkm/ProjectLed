a0ec0f2528646914d086fe3d8bd42053daef33d9
Michal Romaszko <romaszkm@gmail.com>
2018-01-08 22:55:51 +0000

    Fully relative path to config file

--- Python/ProjectLed/conf.py
+++ Python/ProjectLed/conf.py
@@ -1,6 +1,7 @@
 import json
 from RoomClass import Room
-config_file = '../config.json'
+import os
+config_file = os.path.join(os.path.dirname(__file__), '../config.json')
 
 def get_rooms():
     data = json.load(open(config_file))
